5df87320b2b789168f4aaf59333b1bfe9a97eb82
wesstylearg <wesstyle.arg@gmail.com>
2026-01-22 11:41:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const data = {
     { title: "Noche en Capital", client: "Xeon", src: "img/portadas/caribuenas.png"},
     { title: "Te vi", client: "Gondra", src: "img/portadas/te-vi.png" },
     { title: "PUCCA", client: "Niro", src: "img/portadas/Portada Pucca v1 final.png" },
+    { title: "Nuestro Ayer", client: "Siroe", src: "img/portadas/nuestroayer.png" },
     { title: "Mi ataúd", client: "Gondra", src: "img/portadas/mi-ataud.png" },
     { title: "Como te pensé", client: "Waru Lavv", src: "img/portadas/como_te_pense.png" },
+    { title: "0 Movimiento", client: "DCHINX", src: "img/portadas/ceromovimiento.png"},
     { title: "Thanos", client: "Rull Royce", src: "img/portadas/thanos.png" },
     { title: "El genesis", client: "Frannko", src: "img/portadas/genesis.png" },
     { title: "En Mi Mente <3", client: "Siroe", src: "img/portadas/en_mi_mente.jpg" },
@@ -356,6 +358,7 @@ document.getElementById("next").addEventListener("click", () => {
 
 // INICIO: Llama a renderCategories() al cargar la página para mostrar el índice.
 renderCategories();
+
 
 
 
